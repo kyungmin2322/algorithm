feb4af0cfa78641f96a4a196381f2dcbbd4465da
kyungmin2322 <145512842+kyungmin2322@users.noreply.github.com>
2023-11-14 09:15:42 +0900

    Colaboratory를 통해 생성됨

---
+++ avltree.py
@@ -0,0 +1,39 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyMHPH9XpR+Oqn4l4pfeRcbz",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/kyungmin2322/algorithm/blob/main/avltree.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "mFrtIFMbK3wf"
+      },
+      "outputs": [],
+      "source": []
+    }
+  ]
+}
